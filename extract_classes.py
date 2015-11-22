a5f5658cc02e1b6de2268407edd098fc4fe38869
jamesdi1993 <jamesyubai.di@gmail.com>
2015-11-21 20:09:55 -0800

    Extracted images of 4 classes from data

---
+++ extract_classes.py
@@ -0,0 +1,19 @@
+# python libraries
+import collections
+
+# numpy libraries
+import numpy as np
+
+# matplotlib libraries
+import matplotlib.pyplot as plt
+
+# libraries specific to project
+import util
+from cluster import *
+
+def main() :
+   
+
+
+if __name__ == "__main__" :
+   main()
